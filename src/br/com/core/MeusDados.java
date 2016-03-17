c3a2fc2d203ed42b13a43610ade852661dc4923c
daviwesley <davi.wesley@icloud.com>
2016-03-17 00:13:29 -0300

    Projeto adicionado

---
+++ src/br/com/core/MeusDados.java
@@ -0,0 +1,10 @@
+package br.com.core;
+
+public class MeusDados {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ConectorDB minhaConexao = new ConectorDB();
+		minhaConexao.PegarDados();
+	}
+}
