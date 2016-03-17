7b4789bab31f2efff5ce05167e97454de47e30a6
Davi Wesley <davi.wesley@icloud.com>
2016-03-17 00:16:16 -0300

    Arquivo principal

--- src/br/com/core/MeusDados.java
+++ src/br/com/core/MeusDados.java
@@ -4,7 +4,7 @@ public class MeusDados {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		ConectorDB minhaConexao = new ConectorDB();
+		ConectorDB minhaConexao = new ConectorDB();//inicia o construtor da classe
 		minhaConexao.PegarDados();
 	}
 }
